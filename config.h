d5880bf5a83f25de7dbbedb64ad2f5ba529fbb09
Mohamad Safadieh <me@msafadieh.com>
2020-02-18 01:17:45 -0500

    increase borderpx

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 5;
 
 float alpha = 0.8;
 
